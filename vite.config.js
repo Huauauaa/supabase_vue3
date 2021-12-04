cad4a680fe615649f8377c69b182ae35592c10a4
huaweiwei <harvey0379@163.com>
2021-12-04 22:12:56 +0800

    fix: theme

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
-import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
@@ -21,9 +20,6 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
     Components({
       resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
     }),
